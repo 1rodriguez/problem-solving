2bf418c7e4d3b3a5882e758104689a623957b1a0
Juan Rodriguez <jrodri89@uwo.ca>
2020-06-18 09:02:51 -0400

    O(n), O(n) solution

--- leetcode/MyPlayground.playground/Pages/ValidParentheses.xcplaygroundpage/Contents.swift
+++ leetcode/MyPlayground.playground/Pages/ValidParentheses.xcplaygroundpage/Contents.swift
@@ -7,33 +7,28 @@ var str = "Hello, playground"
 //: [Next](@next)
 class Solution {
     
-    func isValid(_ s: String) -> Bool { // TODO: -> Bool
-        let ref : [Character:Character] = ["(" : ")", "{" : "}", "[" : "]"]; // Holds open/close pairs
+    let ref : [Character:Character] = ["(" : ")", "{" : "}", "[" : "]"]; // Holds open/close pairs
+    
+    func isValid(_ s: String) -> Bool {
+    
+        var stack = [Character](); // Append-pop only, array behaves as stack
         
-        var input = Array(s); // Mutability required to pop-out valid substrings
-        var i = 0;
-        
-        if input.count % 2 == 1 { return false }
-        
-        while i < input.count {
-            let char = input[i];
+        if s.count % 2 == 1 { return false; }
 
-            if ref[char] ==  nil && i != 0 { // Expected on the first instance of a closing bracket, keys are all opening brackets
-                if ref[input[i - 1]] == char {
-                    input.remove(at: i - 1);
-                    input.remove(at: i - 1);
-                    i = 0; // reset process
-                } else if input.count == 0 { // array empty, can break and return true
-                    return true;
-                } else { return false } // No closing bracket found
-                
-            } else {i += 1}
+        for (_, char) in s.enumerated() {
+            if self.ref[char] != nil { // If opening parenthesis
+                stack.append(char);
+            } else if stack.count > 0 && self.ref[stack.removeLast()] == char {
+                continue;
+            } else {
+                return false;
+            }
         }
-        return input.count == 0;
+        return stack.count == 0; // Stack should be empty if valid pairs were encountered
     }
 }
 
-let testString = "))";
+let testString = "{}][()";
 
 let someSolution = Solution();
 print(someSolution.isValid(testString));
