e7c8275b65e47f05924707085d588a375ff3b968
Juan Rodriguez <jrodri89@uwo.ca>
2020-06-21 18:23:37 -0400

    other changes

--- leetcode/MyPlayground.playground/Pages/ClimbingStairs.xcplaygroundpage/Contents.swift
+++ leetcode/MyPlayground.playground/Pages/ClimbingStairs.xcplaygroundpage/Contents.swift
@@ -1,7 +1,22 @@
-//: [Previous](@previous)
+class Solution {
+    func climbStairs(_ n: Int) -> Int {
+        /// Implementing via DP bottom-up approach (tabulation)
+        /// Linear time, space
+        var tab = [0]
+       
+        for num in 1...n {
+            if num == 1 {
+                tab.append(1) // Base case, 1 steps
+            } else if num == 2 {
+                tab.append(2) // Base case, 2 steps
+            } else {
+                tab.append(tab[num - 1] + tab[num - 2])
+            }
+        }
+        return tab.removeLast()
+    }
+}
 
-import Foundation
+let someSolution = Solution()
 
-var str = "Hello, playground"
-
-//: [Next](@next)
+print(someSolution.climbStairs(5))
