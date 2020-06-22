25bb9cd59965e161042ff197d6d474b6f452f4ec
Juan Rodriguez <jrodri89@uwo.ca>
2020-06-22 16:14:23 -0400

    O(n) runtime, O(n) space; translate to O(1) space

--- leetcode/MyPlayground.playground/Pages/BuyAndSell.xcplaygroundpage/Contents.swift
+++ leetcode/MyPlayground.playground/Pages/BuyAndSell.xcplaygroundpage/Contents.swift
@@ -1,6 +1,29 @@
 // 121. Best Time to Buy and Sell Stock
 class Solution {
     func maxProfit(_ prices: [Int]) -> Int {
+        var largestDiff : Int = 0
+        var highest : Int? = nil
         
+        let backPrices = prices.reversed()
+        
+        for num in backPrices {
+            if highest == nil {
+                highest = num
+                continue
+            }
+            
+            if num > highest! { // Safe to unwrap, highest has already been assigned a value
+                highest = num
+            } else if num < highest! && highest! - num > largestDiff {
+                largestDiff = highest! - num
+            }
+        }
+        return largestDiff
     }
 }
+
+let example = [7,6,4,3,1]
+
+let someSolution = Solution()
+
+print(someSolution.maxProfit(example))
